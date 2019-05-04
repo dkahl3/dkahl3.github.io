eff5db5c28e8b744dc271363b992e288bf8c6c8b
dkahl3 <43075666+dkahl3@users.noreply.github.com>
2019-05-03 21:26:15 -0500

    Create project_data.js

---
+++ project_data.js
@@ -0,0 +1,39 @@
+var project_data = [
+
+{
+  title: "Bootstrap Website",
+  description: "Bootstrap class project",
+  technologies: ['HTML', 'CSS', 'JavaScript'],
+  date: '02/09/2019',
+  project_url: 'https://dkahl3.github.io/it202-project1/',
+  source_url: 'https://github.com/dkahl3/it202-project1'
+},{
+  title: "AJAX/Google Map PWA",
+  description: "display information from an Ajax call",
+  technologies: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'AJAX'],
+  date: '03/10/2019',
+  project_url: 'https://dkahl3.github.io/IT202Project2/',
+  source_url: 'https://github.com/dkahl3/IT202Project2'
+},{
+  title: "Canvas Game",
+  description: "demonstrate javascript and canvas knowledge",
+  technologies: ['HTML', 'CSS', 'JavaScript'],
+  date: '04/15/2019',
+  project_url: 'https://dkahl3.github.io/IT202-project3/',
+  source_url: 'https://github.com/dkahl3/IT202-project3'
+},{
+  title: "News and Weather Web App",
+  description: "display information about the news and weather in your area",
+  technologies: ['HTML', 'CSS', 'JavaScript','Material Components for the Web', 'jQuery', 'AJAX'],
+  date: '04/29/2019',
+  project_url: 'https://dkahl3.github.io/FinalProject/',
+  source_url: 'https://github.com/dkahl3/FinalProject'
+}
+
+
+
+
+
+
+
+]
